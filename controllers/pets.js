062ca918c69cb8952758830a306a31a375321785
Elizabeth Parnell <beth.parnell@generalassemb.ly>
2024-02-12 13:48:48 -0500

    Shuffle of content

---
+++ controllers/pets.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+
+const Pet = require('../models/pet.js');
+
+// CREATE - POST - /pets
+router.post('/', async (req, res) => {
+    try {
+        const pet = await Pet.create(req.body);
+        res.json({ pet });
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+// READ - GET - /pets
+router.get('/', async (req, res) => {
+    try {
+        const foundPets = await Pet.find();
+        res.json({ pets: foundPets });
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+// READ - GET - /pets/:petId
+router.get('/:petId', async (req, res) => {
+    try {
+        const foundPet = await Pet.findById(req.params.petId);
+        if (!foundPet) {
+            res.status(404);
+            throw new Error('Pet not found.');
+        }
+        res.json({ pet: foundPet });
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+// DELETE - DELETE - /pets/:petId
+router.delete('/:petId', async (req, res) => {
+    try {
+        const foundPet = await Pet.findByIdAndDelete(req.params.petId);
+        if (!foundPet) {
+            res.status(404);
+            throw new Error('Pet not found.');
+        }
+        res.status(204).end();
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+// UPDATE - PUT - /pets/:petId
+router.put('/:petId', async (req, res) => {
+    try {
+        const foundPet = await Pet.findByIdAndUpdate(req.params.petId, req.body);
+        if (!foundPet) {
+            res.status(404);
+            throw new Error('Pet not found.');
+        }
+        const updatedPet = await Pet.findById(req.params.petId);
+        res.json({ pet: updatedPet });
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,78 +4,17 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const petRouter = require('./controllers/pets.js');
+
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-const Fruit = require('./models/fruit.js');
-
 app.use(express.json());
 
-// CREATE - POST - /fruits
-app.post('/fruits', async (req, res) => {
-    try {
-        const fruit = await Fruit.create(req.body);
-        res.json({ fruit });
-    } catch (error) {
-        res.json({ error: error.message });
-    }
-});
-
-// READ - GET - /fruits
-app.get('/fruits', async (req, res) => {
-    try {
-        const foundFruits = await Fruit.find();
-        res.json({ fruits: foundFruits });
-    } catch (error) {
-        res.json({ error: error.message });
-    }
-});
-
-// READ - GET - /fruits/:fruitId
-app.get('/fruits/:fruitId', async (req, res) => {
-    try {
-        const foundFruit = await Fruit.findById(req.params.fruitId);
-        if (!foundFruit) {
-            res.status(404);
-            throw new Error('Fruit not found.');
-        }
-        res.json({ fruit: foundFruit });
-    } catch (error) {
-        res.json({ error: error.message });
-    }
-});
-
-// DELETE - DELETE - /fruits/:fruitId
-app.delete('/fruits/:fruitId', async (req, res) => {
-    try {
-        const foundFruit = await Fruit.findByIdAndDelete(req.params.fruitId);
-        if (!foundFruit) {
-            res.status(404);
-            throw new Error('Fruit not found.');
-        }
-        res.status(204).end();
-    } catch (error) {
-        res.json({ error: error.message });
-    }
-});
-
-// UPDATE - PUT - /fruits/:fruitId
-app.put('/fruits/:fruitId', async (req, res) => {
-    try {
-        const foundFruit = await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
-        if (!foundFruit) {
-            res.status(404);
-            throw new Error('Fruit not found.');
-        }
-        const updatedFruit = await Fruit.findById(req.params.fruitId);
-        res.json({ fruit: updatedFruit });
-    } catch (error) {
-        res.json({ error: error.message });
-    }
-});
+app.use('/pets', petRouter);
 
 app.listen(3000, () => {
     console.log('The express app is ready!');
